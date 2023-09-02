b750f153419017c75f3ed57084e4a80cb12fae41
icy ❄ <62608915+duh@users.noreply.github.com>
2023-09-03 00:18:42 +0100

    Update lastfm-api.py

--- lastfm-api.py
+++ lastfm-api.py
@@ -1,2 +1,4 @@
+# Defines variables and methods for working with the Last.fm API
 API_KEY = "" # Your API key goes here.
 USER_AGENT = "LastFM-Utils script" # This can be changed, but please abide by Last.fm TOS: "Please use an identifiable User-Agent header on all requests. This helps our logging and reduces the risk of you getting banned."
+
