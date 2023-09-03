cf1d70552fc373522a7343a758f6b18ddb6304e0
icy ❄ <62608915+duh@users.noreply.github.com>
2023-09-03 16:23:54 +0100

    Update fmapi.py

--- fmapi.py
+++ fmapi.py
@@ -1,16 +1,16 @@
-# Defines variables and methods for working with the Last.fm API
+# Methods for working with the Last.fm API. Do not touch this file unless you know what you are doing!
 import requests
 import argparse
+from fmcreds import API_KEY, USER_AGENT
 
-API_KEY = " " # Your API key goes here.
-USER_AGENT = "LastFM-Utils script" # This can be changed, but please abide by Last.fm TOS: "Please use an identifiable User-Agent header on all requests. This helps our logging and reduces the risk of you getting banned."
-
+# Validation methods
 def check_positive(value):
     ivalue = int(value)
     if ivalue <= 0:
         raise argparse.ArgumentTypeError("Argument must be a positive int!")
     return ivalue
 
+# API methods
 def get_request(payload):
     payload["api_key"] = API_KEY
     payload["format"] = "json"
