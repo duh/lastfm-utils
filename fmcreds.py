e5fd2a6ee298409b392fabaab236ecf1ed3e44dc
icy ❄ <62608915+duh@users.noreply.github.com>
2023-09-03 16:39:38 +0100

    Delete fmcreds.py

--- fmcreds.py
+++
@@ -1,2 +0,0 @@
-API_KEY = " cbvvbcv" # Your API key goes here.
-USER_AGENT = "LastFM-Utils script" # This can be changed, but please abide by Last.fm TOS: "Please use an identifiable User-Agent header on all requests. This helps our logging and reduces the risk of you getting banned."
